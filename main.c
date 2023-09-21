4e2d65aecc0e8a3b2dfc5b3ef2e4f8aa3901ff7a
ibtissam <ibtissamelhani36@gmail.com>
2023-09-21 15:48:20 +0200

    third commit

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include<time.h>
 
 
 
 typedef struct {
+    int anne;
+    int mois;
     int jour;
-    int heur;
-    int min;
 
-}deadline;
+}date;
 
 typedef struct {
   int id;
   char titre[10];
   char description[100];
-  deadline dead;
+  date deadline;
   int status;
 
 }Tache;
@@ -24,25 +25,27 @@ typedef struct {
 
 Tache tab[100];
 int n, i;
+int contId =0;
 
 void ajouterTache(){
-    n=1;
+
   printf("ajouter une nouvelle tache \n");
-  printf("id : ");
-  scanf("%d", &tab[i].id);
   printf("titre : ");
   scanf("%s", tab[i].titre);
   printf("description : ");
-  scanf(" %29[^\n]", tab[i].description);
+  scanf(" %[^\n]", tab[i].description);
   printf("deadline :\n ");
   printf("jour :");
-  scanf("%d", &tab[i].dead.jour);
+  scanf("%d", &tab[i].deadline.anne);
   printf("mois :");
-  scanf("%d", &tab[i].dead.heur);
-   printf("annee :");
-  scanf("%d", &tab[i].dead.min);
+  scanf("%d", &tab[i].deadline.mois);
+  printf("annee :");
+  scanf("%d", &tab[i].deadline.jour);
   printf("status :\n <1> a realiser \n <2> encore \n <3> finalisee \n ");
   scanf("%d", &tab[i].status);
+    contId++;
+    tab[i].id=contId;
+
     }
 
 
@@ -51,43 +54,31 @@ void ajouterTache(){
 
 void ajouterPlusieur(){
 
-   printf("enter le nombre des taches a ajouter et entrer <0> pour finir : ");
+   printf("enter le nombre des taches a ajouter : ");
    scanf("%d", &n);
-     for(i=0;i<n; i++){
-        printf("tache %d \n", i+1);
-        printf("id : ");
-        scanf("%d", &tab[i].id);
-        printf("titre : ");
-        scanf("%s", &tab[i].titre);
-        printf("description : ");
-        scanf(" %29[^\n]", &tab[i].description);
-        printf("deadline : ");
-        printf("jour :");
-        scanf("%d", &tab[i].dead.jour);
-        printf("mois :");
-        scanf("%d", &tab[i].dead.heur);
-        printf("annee :");
-        scanf("%d", &tab[i].dead.min);
-        printf("status :\n <1> a realiser \n <2> encore \n <1> finalisee \n ");
-        scanf("%d", &tab[i].status);
 
+     for(i=0;i<n; i++){
+       ajouterTache();
    }
    }
+
 
 void afficher(){
-    for(i=0;i<n; i++){
+ for(i=0;i<n; i++){
     printf(" tache %d : \n", tab[i].id);
     printf("titre: %s \n", tab[i].titre);
     printf(" description : %s \n", tab[i].description);
+    printf(" deadline : %d/%d/%d \n", tab[i].deadline.jour, tab[i].deadline.mois, tab[i].deadline.anne);
     printf(" status %d \n\n", tab[i].status);
 
- }
-}
+}}
+
 void triOrdreAlpha(){
   printf("la liste des taches par ordre alphabetique : \n");
   int result;
   Tache c;
   for(i=0;i<n;i++){
+//       tab[i].titre=tolower(tab[i].titre);
       for(int j=i+1;j<n;j++){
 
         result=strcmp(tab[i].titre, tab[j].titre);
@@ -98,14 +89,9 @@ void triOrdreAlpha(){
         }
       }
    }
-    for(i=0;i<n; i++){
-    printf(" tache %d : \n", tab[i].id);
-    printf("titre: %s \n", tab[i].titre);
-    printf(" description : %s \n", tab[i].description);
-    printf(" status %d \n\n", tab[i].status);
-
+   afficher();
  }
-}
+
 
 
 
@@ -117,7 +103,7 @@ void affichage(){
     scanf("%d", &choix2);
     switch(choix2){
 case 1:
-    triOrdreAlpha();
+    afficher();
     break;
 case 2:
     break;
@@ -126,6 +112,46 @@ case 3:
 default:
     printf("entrer un nombre entre 1 ET 3");
     break;
+    }
+
+}
+
+void modification(){
+    int choix3;
+    int a;
+    printf("entrer id de tache a modifier  ");
+    scanf("%d", &a);
+    for(i=0;i<n; i++){
+        if(tab[i].id==a){
+            printf("<1> Modifier la description d'une tache.\n");
+            printf("<2> Modifier le statut d’une tache.\n");
+            printf("<3> Modifier le deadline d’une tache.\n");
+            scanf("%d", &choix3);
+                switch(choix3){
+                    case 1:
+                        printf("entrer la nouvelle description : ");
+                        scanf(" %[^\n]", tab[i].description);
+                        break;
+                    case 2:
+                        printf("status :\n <1> a realiser \n <2> encore \n <3> finalisee \n ");
+                        scanf("%d", &tab[i].status);
+                        break;
+                    case 3:
+                        printf("entrer le nouveau deadline : \n");
+                        printf("jour :");
+                        scanf("%d", &tab[i].deadline.anne);
+                        printf("mois :");
+                        scanf("%d", &tab[i].deadline.mois);
+                        printf("annee :");
+                        scanf("%d", &tab[i].deadline.jour);
+                        break;
+                    default:
+                        printf("entrer un nombre entre 1 ET 3");
+                        break;
+                    }
+        }else{
+        printf("la tache n'existe pas");
+        }
     }
 
 }
@@ -153,7 +179,7 @@ printf("\t[6] Rechercher une tache.\n");
 printf("\t[7] Statistiques.txt\n");
 printf("\t[8] Quitter.\n");
 printf("==============================================================\n");
-printf("tapez votre choix [1-6] \n");
+printf("tapez votre choix [1-6] : ");
 scanf("%d", &choix);
 if(choix==8) break;
 
@@ -165,7 +191,10 @@ case 2:
     ajouterPlusieur();
     break;
 case 3:
-    affichage();
+    afficher();
+    break;
+case 4:
+    modification();
     break;
 default:
     printf("ce choix n'existe pas entrer un nombre entre 1 et 7");
